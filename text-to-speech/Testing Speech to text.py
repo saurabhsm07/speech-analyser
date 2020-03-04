e5027f24d631a2421fc7207e36541dc35f235f71
saurabh <mahambrey.saurabh07@gmail.com>
2020-03-04 22:34:59 +0530

    UPDATE added class structure ADD separate functions for live audio conversion and file based audio conversion added ffmpeg and pydub packages implementation for mp3, m4a to wav conversion

--- text-to-speech/Testing Speech to text.py
+++ text-to-speech/Testing Speech to text.py
@@ -7,49 +7,93 @@ Created on Sat Feb 29 20:45:54 2020
 #%%
 import speech_recognition as sr;
 from os import path;
-
-
-#%%
-# case 1: recognizing and converting live audio using laptop Microphone to text
-
-# step 1: listen to audio file from microphone source
-recognizer_microphone = sr.Recognizer()
-with sr.Microphone() as source:
-    print("start talking");
-    input_audio = recognizer_microphone.listen(source);
-#%%
-# step 2: pring text from audio after analysis
-try:
-    print("What do you mean by :");
-    print(recognizer_microphone.recognize_sphinx(input_audio));
-except sr.UnknownValueError:
-    print("Sphinx could not understand audio");
-except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e));
-
-#%%    
-# case 2: recognizing and converting a audio file to text
-    
-# step 1: load audio file
-audio_files_location = 'F:/Workspace-Personal/Improvements-workspace/semicolons-2020/Speech-to-text-converter/recordings/';
-audio_file_name =  'Recording.m4a';
-
 from pydub import AudioSegment
+from pydub.utils import which
+
+AudioSegment.converter = which("ffmpeg")
+#%%
+
+class Speech_analyser:
+    audio_files_location = 'F:/Workspace-Personal/Improvements-workspace/semicolons-2020/Speech-to-text-converter/recordings/';
+    input_audio_filename =  '';
+    processed_audio_filename = '';
+    speech_to_text_result = '';
+    
+    def set_input_file_name(self, filename):
+        self.input_audio_filename = filename;
+        
+    def convert_m4a_to_wav(self):
+        src = self.input_audio_filename;
+        dest = self.input_audio_filename.split('.')[0] + 'wav';
+        try:                                                 
+            sound = AudioSegment.from_file(path.join(self.audio_files_location,src), format = 'mp4');
+            sound.export(path.join(self.audio_files_location,dest), format = 'wav');
+            self.processed_audio_filename = dest;
+        except:
+            self.processed_audio_filename = "file_error";
+        
+    def convert_mp3_to_wav(self):
+        src = self.input_audio_filename;
+        dest = self.input_audio_filename.split('.')[0] + 'wav';
+        try:                                                 
+            sound = AudioSegment.from_file(path.join(self.audio_files_location,src), format = 'mp3');
+            sound.export(path.join(self.audio_files_location,dest), format = 'wav');
+            self.processed_audio_filename = dest;
+        except:
+            self.processed_audio_filename = "file_error";
+    
+    def analyse_live_audio(self):
+        recognizer_microphone = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("start talking");
+            microphone_audio = recognizer_microphone.listen(source);
+        self.convert_speech_to_text(microphone_audio, recognizer_microphone);
+    
+    def analyse_audio_file(self):
+        format = self.input_audio_filename.split(".")[1];
+        if format == 'wav':
+            self.analyse_file()
+        elif format == 'mp3':
+            self.convert_mp3_to_wav();
+            self.analyse_file();
+        elif format == 'm4a':
+            self.convert_m4a_to_wav();
+            self.analyse_file();
+        else:
+            print("can't decode file of format = {}".format(format));
+    
+    def analyse_file(self):
+        if(self.processed_audio_filename == "file_error"):
+            print("error in file conversion")
+        else:
+            recognizer_audiofile = sr.Recognizer();
+            audio_file = path.join(self.audio_files_location, self.processed_audio_filename);
+            with sr.AudioFile(audio_file) as source:
+                file_audio = recognizer_audiofile.record(source);
+            self.convert_speech_to_text(file_audio, recognizer_audiofile);
+        
+    def convert_speech_to_text(self, audio, recognizer):
+        try:
+            print("What do you mean by :");
+            print(recognizer.recognize_sphinx(audio));
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio");
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e));
+
+    def audio_to_text(self):
+        if self.speech_to_text_result == "":
+            print("nothing analysed");
+        else:
+            print("speech analysis results :");
+            print(self.speech_to_text_result);
+
+
 
 #%%
-def conver_m4a_to_wav():
-    src = "Recording.mp3"
-    dest = "test.wav"                                                 
-    sound = AudioSegment.from_mp3(path.join(audio_files_location,src))
-    sound.export(path.join(audio_files_location,dest), format="wav")
-
-#%%    
-AUDIO_FILE = path.join(audio_files_location, audio_file_name);
+analyser = Speech_analyser()                    #initialize analyser object
 #%%
-conver_m4a_to_wav()
+analyser.set_input_file_name('Recording.m4a')   #set file name to be processed
 #%%
-# step 2: recognizer entire saved audio file 
-recognizer_audiofile = sr.Recognizer()
-with sr.AudioFile(AUDIO_FILE) as source:
-    audio = recognizer_audiofile.record(source)
+analyser.analyse_audio_file()                   #analyse audio function
 #%%
